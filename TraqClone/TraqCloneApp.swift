fae0a464b5406d93bec6c4bc7156eefd15a1978b
Ras96 <asymptote.k.k@gmail.com>
2022-08-19 02:02:42 +0900

    :tada: make Project

---
+++ TraqClone/TraqCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TraqCloneApp.swift
+//  TraqClone
+//
+//  Created by Kira Kawai on 2022/08/19.
+//
+
+import SwiftUI
+
+@main
+struct TraqCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
