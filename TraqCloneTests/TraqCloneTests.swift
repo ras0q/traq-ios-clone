108955203f8d5d102d0a1ff458312b63991f4f2c
Ras96 <asymptote.k.k@gmail.com>
2022-08-19 02:13:08 +0900

    :art: introduce swiftformat & swiftlint

--- TraqCloneTests/TraqCloneTests.swift
+++ TraqCloneTests/TraqCloneTests.swift
@@ -5,11 +5,10 @@
 //  Created by Kira Kawai on 2022/08/19.
 //
 
-import XCTest
 @testable import TraqClone
+import XCTest
 
 class TraqCloneTests: XCTestCase {
-
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
@@ -28,9 +27,8 @@ class TraqCloneTests: XCTestCase {
 
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
-        self.measure {
+        measure {
             // Put the code you want to measure the time of here.
         }
     }
-
 }
