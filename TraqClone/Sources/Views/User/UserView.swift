1474343ef5ee7797cd6b052e4fda02d37bb7eb45
Ras96 <asymptote.k.k@gmail.com>
2022-09-13 01:00:41 +0900

    :truck: move userElement into UserView

--- TraqClone/Sources/Views/User/UserView.swift
+++ TraqClone/Sources/Views/User/UserView.swift
@@ -1,6 +1,8 @@
 import ComposableArchitecture
+import SDWebImageSwiftUI
 import Stores
 import SwiftUI
+import Traq
 
 public struct UserView: View {
     private let store: ServiceCore.Store
@@ -12,13 +14,8 @@ public struct UserView: View {
     public var body: some View {
         WithViewStore(store) { viewStore in
             NavigationView {
-                List(
-                    viewStore.user.users
-                        .filter { !$0.bot && $0.state == .active }
-                        .sorted { $0.name.lowercased() < $1.name.lowercased() },
-                    id: \.id
-                ) { user in
-                    UserElement(user: user)
+                List(getUsers(from: viewStore.user), id: \.id) { user in
+                    userElementButton(user)
                 }
                 .toolbar {
                     ToolbarItem(placement: .navigationBarLeading) {
@@ -26,6 +23,35 @@ public struct UserView: View {
                             .font(.largeTitle)
                             .bold()
                     }
+                }
+            }
+        }
+    }
+
+    private func getUsers(from userState: UserCore.State) -> [TraqAPI.User] {
+        userState.users
+            .filter { !$0.bot && $0.state == .active }
+            .sorted { $0.name.lowercased() < $1.name.lowercased() }
+    }
+
+    private func userElementButton(_ user: TraqAPI.User) -> some View {
+        Button(action: {}) {
+            HStack {
+                WebImage(url: URL(string: "\(TraqAPI.basePath)/public/icon/\(user.name)"), isAnimating: .constant(true))
+                    .onFailure { error in
+                        print(error.localizedDescription)
+                    }
+                    .resizable()
+                    .placeholder(Image(systemName: "person.crop.circle"))
+                    .indicator(.activity) // Activity Indicator
+                    .clipShape(Circle())
+                    .frame(width: 35, height: 35, alignment: .leading)
+                VStack(alignment: .leading) {
+                    Text("\(user.displayName)")
+                        .foregroundColor(.black)
+                    Text("@\(user.name)")
+                        .font(.footnote)
+                        .foregroundColor(.gray)
                 }
             }
         }
