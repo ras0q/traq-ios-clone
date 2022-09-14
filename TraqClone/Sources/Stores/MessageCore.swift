2702ccbc128a4c04a67e4077496fb3545eec9ec6
Ras96 <asymptote.k.k@gmail.com>
2022-09-14 10:48:13 +0900

    :sparkles: add fetchMessages action

--- TraqClone/Sources/Stores/MessageCore.swift
+++ TraqClone/Sources/Stores/MessageCore.swift
@@ -7,6 +7,7 @@ public enum MessageCore {
     public typealias Reducer = ComposableArchitecture.Reducer<State, Action, Environment>
 
     public struct State: Equatable {
+        public var channelMesssages: [UUID: [TraqAPI.Message]] = .init()
         public var messages: [TraqAPI.Message] = .init()
         public var messageDictionary: [UUID: TraqAPI.Message] { messages.toDictionary(id: \.id) }
 
@@ -14,6 +15,8 @@ public enum MessageCore {
     }
 
     public enum Action: Equatable {
+        case fetchMessages(channelId: UUID)
+        case fetchMessagesResponse(TaskResult<([TraqAPI.Message], channelId: UUID)>)
         case fetchMessage(UUID, isCiting: Bool)
         case fetchMessageResponse(TaskResult<(TraqAPI.Message, isCiting: Bool)>)
 
@@ -28,20 +31,46 @@ public enum MessageCore {
 
     public static let reducer = Reducer { state, action, _ in
         switch action {
+        case let .fetchMessages(channelId: channelId):
+            return .task {
+                await .fetchMessagesResponse(
+                    TaskResult {
+                        let messages = try await TraqAPI.MessageAPI.getMessages(channelId: channelId)
+                        return (messages, channelId: channelId)
+                    }
+                )
+            }
+        case let .fetchMessagesResponse(.success((messages, channelId: channelId))):
+            if var channelMessages = state.channelMesssages[channelId] {
+                channelMessages = channelMessages.filter { !messages.map(\.id).contains($0.id) }
+                channelMessages.append(contentsOf: messages)
+                state.channelMesssages[channelId] = channelMessages
+            } else {
+                state.channelMesssages[channelId] = messages
+            }
+            return .none
+        case let .fetchMessagesResponse(.failure(error)):
+            print("failed to fetch messages: \(error)")
+            return .none
         case let .fetchMessage(messageId, isCiting: isCiting):
             return .task {
                 await .fetchMessageResponse(
                     TaskResult {
                         let message = try await TraqAPI.MessageAPI.getMessage(messageId: messageId)
-                        return (message: message, isCiting: isCiting)
+                        return (message, isCiting: isCiting)
                     }
                 )
             }
         case let .fetchMessageResponse(.success((message, isCiting: _))):
-            guard !state.messages.contains(where: { $0.id == message.id }) else {
-                return .none
+            let channelId = message.channelId
+            if let channelMessages = state.channelMesssages[channelId] {
+                guard !channelMessages.contains(where: { $0.id == message.id }) else {
+                    return .none
+                }
+                state.channelMesssages[channelId]!.append(message)
+            } else {
+                state.channelMesssages[channelId] = [message]
             }
-            state.messages.append(message)
             return .none
         case let .fetchMessageResponse(.failure(error)):
             print("failed to fetch messages: \(error)")
